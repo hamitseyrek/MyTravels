a1e1bbfbb3c1ed6ff3f015295300d4dd9baef1fe
hamitseyrek <hamitseyrek@gmail.com>
2022-01-17 21:20:10 +0300

    Initial Commit

---
+++ MyTravels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTravels
+//
+//  Created by Hamit Seyrek on 17.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
