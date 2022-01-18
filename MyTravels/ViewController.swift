3f84d314986deb00a77898c82828ef3340c74624
hamitseyrek <hamitseyrek@gmail.com>
2022-01-18 21:40:25 +0300

    add HomeVc1

---
+++ MyTravels/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MyTravels
+//
+//  Created by Hamit Seyrek on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
