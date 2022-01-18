b149690d98c42634cc7b5a52ab579449332a0a60
hamitseyrek <hamitseyrek@gmail.com>
2022-01-18 21:42:06 +0300

    configure ViewController

--- MyTravels/ViewController.swift
+++ MyTravels/ViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //navigator configuration
+        navigationItem.title = "My Travels"
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toMapVCS", sender: nil)
     }
-    */
-
+    
+    //TableView configuration
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Ben"
+        cell.contentConfiguration = content
+        return cell
+    }
 }
